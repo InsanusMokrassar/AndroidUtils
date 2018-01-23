69dd58e8dd4acbf55da96e467917b7c8d18c7bdd
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2018-01-23 10:38:48 +0800

    add Context#toasts

--- CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/extensions/Context.kt
+++ CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/extensions/Context.kt
@@ -3,9 +3,48 @@ package com.github.insanusmokrassar.CommonAndroidUtils.front.extensions
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 
 fun Context.createAndStartActivity(activityClass: Class<out Activity>, flags: Int = 0) {
     val intent = Intent(this, activityClass)
     intent.flags = flags
     startActivity(intent)
+}
+
+fun Context.defaultShortToast(
+        text: String
+) {
+    launch (UI) {
+        Toast.makeText(
+                this@defaultShortToast,
+                text,
+                Toast.LENGTH_SHORT
+        ).show()
+    }
+}
+
+fun Context.defaultShortToast(
+        textRes: Int
+) {
+    defaultShortToast(getString(textRes))
+}
+
+fun Context.defaultLongToast(
+        text: String
+) {
+    launch (UI) {
+        Toast.makeText(
+                this@defaultLongToast,
+                text,
+                Toast.LENGTH_LONG
+        ).show()
+    }
+}
+
+fun Context.defaultLongToast(
+        textRes: Int
+) {
+    defaultLongToast(getString(textRes))
 }
