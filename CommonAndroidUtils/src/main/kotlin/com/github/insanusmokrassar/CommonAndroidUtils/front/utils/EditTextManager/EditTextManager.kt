f5bc037b5d8c7fa4afa54b9d459d2ad47da8a6c2
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2018-01-27 22:19:04 +0800

    text changed validator

--- CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/utils/EditTextManager/EditTextManager.kt
+++ CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/utils/EditTextManager/EditTextManager.kt
@@ -1,17 +1,20 @@
 package com.github.insanusmokrassar.CommonAndroidUtils.front.utils.EditTextManager
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 
 open class EditTextManager<T>(
-        private val view: EditText,
+        val view: EditText,
         private val validChecker: (T, EditTextManager<T>) -> Boolean = {
             _, _ ->
             true
         },
         private val textTransformationSet: (T, EditTextManager<T>) -> String,
-        private val textTransformationGet: (String, EditTextManager<T>) -> T
+        private val textTransformationGet: (String, EditTextManager<T>) -> T,
+        private val textChangedValidator: ((String) -> Boolean)? = null
 ) {
     var text: T?
         get() = textTransformationGet(
@@ -51,5 +54,37 @@ open class EditTextManager<T>(
                 view.error = value
             }
         }
+
+    init {
+        textChangedValidator ?. let {
+            view.addTextChangedListener(
+                StandardEditTextManagerWatcher(
+                        it
+                )
+            )
+        }
+    }
+}
+
+private class StandardEditTextManagerWatcher(
+        private val validator: (String) -> Boolean
+): TextWatcher {
+    private var oldText: String? = null
+
+    override fun afterTextChanged(editable: Editable) {
+        if (!validator(editable.toString())) {
+            editable.replace(
+                    0,
+                    editable.length,
+                    oldText
+            )
+        }
+    }
+
+    override fun beforeTextChanged(oldText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        this.oldText = oldText ?. toString()
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 }
 
--- CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/utils/EditTextManager/StringEditTextManager.kt
+++ CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/utils/EditTextManager/StringEditTextManager.kt
@@ -6,7 +6,8 @@ open class StringEditTextManager(
         view: EditText,
         validChecker: (String, EditTextManager<String>) -> Boolean = { _, _ -> true },
         textTransformationSet: (String, EditTextManager<String>) -> String = { it, _ -> it },
-        textTransformationGet: (String, EditTextManager<String>) -> String = { it, _ -> it }
+        textTransformationGet: (String, EditTextManager<String>) -> String = { it, _ -> it },
+        editTextValidator: (String) -> Boolean = { true }
 ) : EditTextManager<String>(
-        view, validChecker, textTransformationSet, textTransformationGet
+        view, validChecker, textTransformationSet, textTransformationGet, editTextValidator
 )
