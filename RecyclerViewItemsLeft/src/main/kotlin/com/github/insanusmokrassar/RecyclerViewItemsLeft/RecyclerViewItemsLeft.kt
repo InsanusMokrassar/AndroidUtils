78daf8af1070d0e47ca61277cfdcbc3a551043cf
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2018-02-05 17:19:00 +0800

    add RecyclerViewItemsLeft

---
+++ RecyclerViewItemsLeft/src/main/kotlin/com/github/insanusmokrassar/RecyclerViewItemsLeft/RecyclerViewItemsLeft.kt
@@ -0,0 +1,48 @@
+package com.github.insanusmokrassar.RecyclerViewItemsLeft
+
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import java.util.*
+
+typealias LeftItemsCallback = (Int) -> Unit
+typealias LeftItemsSubscription = Observable<Int>
+
+private val weakLeftItemsMap = WeakHashMap<RecyclerView, LeftItemsSubscription>()
+
+fun RecyclerView.subscribeItemsLeft(
+        callback: LeftItemsCallback,
+        leftPagesCount: Int
+) {
+    if (leftPagesCount < 0) {
+        throw IllegalArgumentException("leftPagesCount must be positive")
+    }
+    weakLeftItemsMap[this] ?.let {
+        it.filter {
+            it <= leftPagesCount
+        }.subscribe(callback)
+    } ?:let {
+        weakLeftItemsMap[this] = PublishSubject.create()
+        subscribeItemsLeft(callback, leftPagesCount)
+    }
+}
+
+private class LinearLayoutManagerLeftItemsListener(
+        private val layoutManager: LinearLayoutManager,
+        private val callback: LeftItemsCallback
+) : RecyclerView.OnScrollListener() {
+    private var lastVisible: Int = -1
+        set(value) {
+            if (field != value) {
+                field = value
+                val maxPosition = layoutManager.itemCount - 1
+                callback(maxPosition - lastVisible)
+            }
+        }
+
+    override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        lastVisible = layoutManager.findLastVisibleItemPosition()
+    }
+}
