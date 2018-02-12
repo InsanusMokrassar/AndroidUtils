3049f93ac709bc6acadda051ade519c4a7f8b9d0
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-02-12 12:06:50 +0800

    Revert "update template of publish"
    This reverts commit d3d56d053f0880d479b7e70bb9b1ad671e8e726f.

--- publish_template.sh
+++ publish_template.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-#################################################
-#
-# IT IS JUST TEMPLATE
-# IF YOU WANT TO USE THIS PUBLISH FILE - REPLASE
-# BINTRAY_USER AND BINTRAY_KEY IN NEW publish.sh
-# FILE
 
-# ALL ARGUMENTS WILL DETECTING AS NAMES OF MODULES
-for MODULE_NAME in $@ ; do
-    echo Publish $MODULE_NAME module
-    ./gradlew ${MODULE_NAME}:clean ${MODULE_NAME}:build ${MODULE_NAME}:bintrayUpload -PbintrayUser=BINTRAY_USERNAME -PbintrayKey=BINTRAY_KEY -PdryRun=false
-done
+# Here first argument will be detected as name of module
+PROJECT_NAME=$1
+./gradlew ${PROJECT_NAME}:clean ${PROJECT_NAME}:build ${PROJECT_NAME}:bintrayUpload -PbintrayUser=BINTRAY_USERNAME -PbintrayKey=BINTRAY_KEY -PdryRun=false
