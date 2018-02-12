0a0e3f5f7111b703308dc729bd3476bf8ab21371
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-02-12 12:06:59 +0800

    Revert "add publishing"
    This reverts commit 2d7e490b90fdc7c403b9138220e9bf594f72b7bd.

--- CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/helpers/AlertDialog.kt
+++ CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/helpers/AlertDialog.kt
@@ -8,6 +8,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.text.InputType
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.github.insanusmokrassar.CommonAndroidUtils.R
 
--- publish_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Here first argument will be detected as name of module
-PROJECT_NAME=$1
-./gradlew ${PROJECT_NAME}:clean ${PROJECT_NAME}:build ${PROJECT_NAME}:bintrayUpload -PbintrayUser=BINTRAY_USERNAME -PbintrayKey=BINTRAY_KEY -PdryRun=false
