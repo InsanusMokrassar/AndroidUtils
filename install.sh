0a82ee59b605da25486f3316377885e57990d4b2
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-02-21 00:36:18 +0800

    optimise install script

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-./gradlew clean cleanBuildCache CommonAndroidUtils:install RecyclerViewAdapter:install RecyclerViewItemsLeft:install SimpleAndroidViewsManagers:install
-./gradlew clean cleanBuildCache SimpleAndroidORM:install SimpleAndroidRequests:install SimpleAndroidAppsHelper:install
+function assert_success() {
+    "${@}"
+    local status=${?}
+    if [ ${status} -ne 0 ]; then
+        echo "### Error ${status} at: ${BASH_LINENO[*]} ###"
+        exit ${status}
+    fi
+}
+
+assert_success ./gradlew clean cleanBuildCache CommonAndroidUtils:install RecyclerViewAdapter:install RecyclerViewItemsLeft:install SimpleAndroidViewsManagers:install
+assert_success ./gradlew clean cleanBuildCache SimpleAndroidORM:install SimpleAndroidRequests:install SimpleAndroidAppsHelper:install
