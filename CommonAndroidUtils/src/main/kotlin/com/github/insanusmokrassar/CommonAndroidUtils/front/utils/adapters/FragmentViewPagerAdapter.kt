ebc4918eabe22a5a9ddd7870c3ac96401261641b
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-05-08 09:53:34 +0800

    add fix for very old problem with FragmentViewPagerAdapter

--- CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/utils/adapters/FragmentViewPagerAdapter.kt
+++ CommonAndroidUtils/src/main/kotlin/com/github/insanusmokrassar/CommonAndroidUtils/front/utils/adapters/FragmentViewPagerAdapter.kt
@@ -3,6 +3,9 @@ package com.github.insanusmokrassar.CommonAndroidUtils.front.utils.adapters
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import android.os.Parcelable
+
+
 
 class FragmentViewPagerAdapter(
         private val fragments: List<Fragment>,
@@ -17,4 +20,23 @@ class FragmentViewPagerAdapter(
                 getItem(position)
         )
     }
+
+    //-----------------------for uncaching oldest fragments
+    override fun saveState(): Parcelable? {
+        uncacheFragments()
+        return null
+    }
+
+    private fun uncacheFragments() {
+        fragments.forEach {
+            fragment ->
+            try {
+                fragment.fragmentManager ?. beginTransaction() ?.let {
+                    it.remove(fragment)
+                    it.commit()
+                }
+            } catch (e: NullPointerException) { }
+        }
+    }
+
 }
